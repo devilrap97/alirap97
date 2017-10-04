aa9e9ab8bb18f5483a3ae624ae98a20a36a00aae
devilrap <alirap410@gmail.com>
2017-10-04 05:14:06 +0300

    Add files via upload

---
+++ plugins/ar-ASD_KARBALA.lua
@@ -0,0 +1,23 @@
+--[[ 
+▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀          
+▀▄ ▄▀                                      ▀▄ ▄▀ 
+▀▄ ▄▀    BY SAJJAD NOORI                   ▀▄ ▄▀ 
+▀▄ ▄▀     BY SAJAD NOORI (@SAJJADNOORI)    ▀▄ ▄▀ 
+▀▄ ▄▀ JUST WRITED BY SAJJAD NOORI          ▀▄ ▄▀   
+▀▄ ▄▀          Dev  : المطور               ▀▄ ▄▀ 
+▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀
+--]]
+
+do
+
+function run(msg, matches)
+ return '❣ Source 🛢 zain.aliraqex 📁\n❣ Version 📋 v1.2\n❣ Location 💻\n \nhttps://github.com/zain211/zain.aliraqex.git \n❣ #Dev 🕵🔧 : \n❣ #Dev : @AILXXZ \n❣ #Dev_bot @Zain121bot \n❣ #Dev_Channel @zaintyh'
+end
+return {
+  patterns = {
+    "^version"
+  }, 
+  run = run 
+}
+
+end
---
+++ plugins/ar-ASD_KARBALA3.lua
@@ -0,0 +1,33 @@
+--[[ 
+▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀          
+▀▄ ▄▀                                      ▀▄ ▄▀ 
+▀▄ ▄▀    BY SAJJAD NOORI                   ▀▄ ▄▀ 
+▀▄ ▄▀     BY SAJAD NOORI (@AILXXZ)         ▀▄ ▄▀ 
+▀▄ ▄▀ JUST WRITED BY SAJJAD NOORI          ▀▄ ▄▀   
+▀▄ ▄▀          dev1  : dev                 ▀▄ ▄▀ 
+▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀
+--]]
+do
+
+function run(msg, matches)
+return [[
+
+االبوت الذي يعمل على مجوعات السوبر 🔸
+
+يعمل البوت على مجموعات سوبر تصل الى5k عضو 🔷
+
+     ≪تم صنع البوت بواسطة المطور≫
+                      『 @AILXXZ 』
+            🔹#AILXXZ
+]]
+end
+
+return {
+description = "Shows bot q", 
+usage = "spam Shows bot q",
+patterns = {
+"^(dev)$",
+},
+run = run 
+}
+end
